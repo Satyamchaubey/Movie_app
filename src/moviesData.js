6c85d5c6dba3eb8ccb77b9bae6afa200d0ab11a5
Satyamchaubey <115795299+Satyamchaubey@users.noreply.github.com>
2023-12-07 21:45:21 +0530

    Update moviesData.js

--- src/moviesData.js
+++ src/moviesData.js
@@ -1,10 +1,10 @@
  export const movies = [
     {
       id: 1,
-      title: "Lapeta Lapeti",
+      title: "Red(South Indian)",
       plot: "Iski Topi Uske Sar Earth's mightiest heroes must come together and learn to fight.",
       poster:
-      "8bIWELEgwSQ",
+      "6UeLU0uWGbU?si=qXmCvj_SpO5bgVHk",
       price: "free",
       rating: 8.9,
       stars: 0,
@@ -27,10 +27,10 @@
     },
     {
       id: 3,
-      title: "Dark Knight",
+      title: "Check(South Indian)",
       plot: "Iski Topi Uske Sar Earth's mightiest heroes must come together and learn to fight.",
       poster:
-      "xSRMwwm9vlQ?si=ZeixehDEsXQf_7QP",
+      "NpJ6lnbgi_s?si=tD90w9hSDRH7GWhM",
       rating: "9.0",
       price: "free",
       stars: 0,
@@ -44,6 +44,71 @@
       plot: "Iski Topi Uske Sar Earth's mightiest heroes must come together and learn to fight.",
       poster:
         "Xvukp6FpUVU?si=P1UbP-VKnDFvLJlc",
+      rating: "7.9",
+      price: "free",
+      stars: 0,
+      fav: false,
+      cart: false,
+      
+    },
+    {
+      id: 5,
+      title: "Tiger3(Bollywood)",
+      plot: "Iski Topi Uske Sar Earth's mightiest heroes must come together and learn to fight.",
+      poster:
+        "X_63qHrBQDM?si=ixrG0ozypx--1fI6",
+      rating: "7.9",
+      price: "free",
+      stars: 0,
+      fav: false,
+      cart: false,
+      
+    },
+    {
+      id: 6,
+      title: "Leo(South Indian)",
+      plot: "Iski Topi Uske Sar Earth's mightiest heroes must come together and learn to fight.",
+      poster:
+        "YvRwmMevROY?si=NhNkV-yTP5ls3BTj",
+      rating: "7.9",
+      price: "free",
+      stars: 0,
+      fav: false,
+      cart: false,
+      
+    },
+    {
+      id: 7,
+      title: "Furkey Return(Bollywood)",
+      plot: "Iski Topi Uske Sar Earth's mightiest heroes must come together and learn to fight.",
+      poster:
+        "JVfSkgsvHJs?si=0YgW8RO0ml9VGRPW",
+      rating: "7.9",
+      price: "free",
+      stars: 0,
+      fav: false,
+      cart: false,
+      
+    },
+    {
+      id: 8,
+      title: "Custody(South Indian)",
+      plot: "Iski Topi Uske Sar Earth's mightiest heroes must come together and learn to fight.",
+      poster:
+        "KavrRIh0ScQ?si=W8iZSb4HtKG_M1SB",
+      rating: "7.9",
+      price: "free",
+      stars: 0,
+      fav: false,
+      cart: false,
+      
+    },
+    {
+      id: 89,
+      title: "Michael(South Indian)",
+      plot: "Iski Topi Uske Sar Earth's mightiest heroes must come together and learn to fight.",
+      poster:
+        "NU1PqiUyCfA?si=Q1q7NdIPHgbZWmXZ",
       rating: "7.9",
       price: "free",
       stars: 0,
